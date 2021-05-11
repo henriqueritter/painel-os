c73b3782027996f35631d70a697574fe1a7191f3
Henrique Ritter <henrique-ritter@hotmail.com>
2021-05-10 22:54:13 -0300

    OS Panel get Json Data OK

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,96 @@ interface IOrders {
   DEFEITOS?: string;
 }
 
+//BACKUP GETDATA
+// const getData = () => {
+//   fetch("BORR.JSON", {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//   })
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (myJson) {
+//       // console.log(myJson);
+//       setBorr(myJson);
+//     });
+//   console.log(borr[1]);
+// };
+
+// useEffect(() => {
+//   getData();
+// }, []);
+
 function App() {
   const [borr, setBorr] = useState<IOrders[]>([]);
+  const [elet, setElet] = useState<IOrders[]>([]);
+  const [fupi, setFupi] = useState<IOrders[]>([]);
+  const [lubr, setLubr] = useState<IOrders[]>([]);
+  const [meca, setMeca] = useState<IOrders[]>([]);
+  const [ret, setRet] = useState<IOrders[]>([]);
+  const [tapo, setTapo] = useState<IOrders[]>([]);
 
   // receive data
   const getData = () => {
-    fetch("BORR.JSON", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
+    fetch("BORR.JSON", {})
       .then(function (response) {
-        console.log(response);
         return response.json();
       })
       .then(function (myJson) {
         // console.log(myJson);
         setBorr(myJson);
       });
-    console.log(borr[1]);
+
+    fetch("ELET.JSON", {})
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setElet(myJson);
+      });
+
+    fetch("FUPI.JSON", {})
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setFupi(myJson);
+      });
+
+    fetch("LUBR.JSON", {})
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setLubr(myJson);
+      });
+
+    fetch("MECA.JSON", {})
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setMeca(myJson);
+      });
+
+    fetch("RET.JSON", {})
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setRet(myJson);
+      });
+
+    fetch("TAPO.JSON", {})
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setTapo(myJson);
+      });
   };
 
   useEffect(() => {
@@ -46,126 +116,110 @@ function App() {
           {/* div container-content precisa ter 2 colunas e 3 linhas OU 2 colunas e duas linhas apenas e deixa a DIV de retorno fora dessa abaixo da div container-content */}
           <div className="container-content">
             <div className="container-orders" id="BOR">
-              <p>Borracharia</p>
+              <p>Borracharia - Qtd: {borr.length}</p>
               <div className="orders">
                 {borr.map((item) => {
-                  console.log(item.DEFEITOS);
                   return (
                     <ul key={item.OS}>
-                      <span className="span-os">{item.OS}</span> {item.PREFIXO}{" "}
-                      {item.DT_ABER} {item.DEFEITOS}
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
                     </ul>
                   );
                 })}
               </div>
             </div>
             <div className="container-orders" id="ELE">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <p>Elétrica - Qtd: {elet.length}</p>
+              <div className="orders">
+                {elet.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
+              </div>
             </div>
             <div className="container-orders" id="FUN">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <p>Funilaria - Qtd: {fupi.length}</p>
+              <div className="orders">
+                {fupi.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
+              </div>
             </div>
             <div className="container-orders" id="LUB">
-              <p>Setor Ordem</p>
+              <p>Lubrificação - Qtd: {lubr.length}</p>
               <div className="orders">
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
+                {lubr.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
               </div>
             </div>
             <div className="container-orders" id="MEC">
-              <p>Setor Ordem</p>
+              <p>Mecânica - Qtd: {meca.length}</p>
               <div className="orders">
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
-                <br />
-                <span>Nº OS Prefix Time Description</span>
+                {meca.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
               </div>
             </div>
             <div className="container-orders" id="TAP">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <p>Tapeçaria - Qtd: {tapo.length}</p>
+              <div className="orders">
+                {tapo.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
+              </div>
             </div>
           </div>
-          <div className="container-orders" id="RET">
-            <p>OS Retorno</p>
+          <div className="container-orders-footer" id="RET">
+            <p>OS Retorno - Qtd: {ret.length}</p>
+            <div className="orders">
+              {ret.map((item) => {
+                return (
+                  <ul key={item.OS}>
+                    <span className="span-os">{item.OS}</span>
+                    <span className="span-prefixo">{item.PREFIXO}</span>
+                    <span className="span-dt_aber">{item.DT_ABER}</span>
+                    <span className="span-defeito">{item.DEFEITOS}</span>
+                  </ul>
+                );
+              })}
+            </div>
           </div>
         </div>
         <div className="container-groups">
@@ -173,32 +227,110 @@ function App() {
           {/* div container-content precisa ter 2 colunas e 3 linhas OU 2 colunas e duas linhas apenas e deixa a DIV de retorno fora dessa abaixo da div container-content */}
           <div className="container-content">
             <div className="container-orders" id="BOR">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <p>Borracharia - Qtd: {borr.length}</p>
+              <div className="orders">
+                {borr.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
+              </div>
             </div>
             <div className="container-orders" id="ELE">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <p>Elétrica - Qtd: {borr.length}</p>
+              <div className="orders">
+                {borr.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
+              </div>
             </div>
             <div className="container-orders" id="FUN">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <p>Funilaria - Qtd: {borr.length}</p>
+              <div className="orders">
+                {borr.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
+              </div>
             </div>
             <div className="container-orders" id="LUB">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <p>Lubrificação - Qtd: {borr.length}</p>
+              <div className="orders">
+                {borr.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
+              </div>
             </div>
             <div className="container-orders" id="MEC">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <p>Mecânica - Qtd: {borr.length}</p>
+              <div className="orders">
+                {borr.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
+              </div>
             </div>
             <div className="container-orders" id="TAP">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <p>Tapeçaria - Qtd: {borr.length}</p>
+              <div className="orders">
+                {borr.map((item) => {
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span>
+                      <span className="span-prefixo">{item.PREFIXO}</span>
+                      <span className="span-dt_aber">{item.DT_ABER}</span>
+                      <span className="span-defeito">{item.DEFEITOS}</span>
+                    </ul>
+                  );
+                })}
+              </div>
             </div>
           </div>
-          <div className="container-orders" id="RET">
-            <p>OS Retorno</p>
+          <div className="container-orders-footer" id="RET">
+            <p>OS Retorno - Qtd: {borr.length}</p>
+            <div className="orders">
+              {borr.map((item) => {
+                return (
+                  <ul key={item.OS}>
+                    <span className="span-os">{item.OS}</span>
+                    <span className="span-prefixo">{item.PREFIXO}</span>
+                    <span className="span-dt_aber">{item.DT_ABER}</span>
+                    <span className="span-defeito">{item.DEFEITOS}</span>
+                  </ul>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
