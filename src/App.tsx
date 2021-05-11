21153c773f8d13702c32bd2007377fe7f020e845
Henrique Ritter <henrique-ritter@hotmail.com>
2021-05-10 22:06:20 -0300

    GetData from JSON File

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/global.css";
 
+interface IOrders {
+  SETOR: string;
+  OS: number;
+  PREFIXO: string;
+  DT_ABER: string;
+  DEFEITOS?: string;
+}
+
 function App() {
-  // useEffect(() => {
-  //   const elem = document.getElementById("BOR");
-  //   elem.scrollTop = elem?.scrollHeight;
-  // }, []);
+  const [borr, setBorr] = useState<IOrders[]>([]);
+
+  // receive data
+  const getData = () => {
+    fetch("BORR.JSON", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        // console.log(myJson);
+        setBorr(myJson);
+      });
+    console.log(borr[1]);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
       {/* div container = flex column */}
@@ -17,6 +46,28 @@ function App() {
           {/* div container-content precisa ter 2 colunas e 3 linhas OU 2 colunas e duas linhas apenas e deixa a DIV de retorno fora dessa abaixo da div container-content */}
           <div className="container-content">
             <div className="container-orders" id="BOR">
+              <p>Borracharia</p>
+              <div className="orders">
+                {borr.map((item) => {
+                  console.log(item.DEFEITOS);
+                  return (
+                    <ul key={item.OS}>
+                      <span className="span-os">{item.OS}</span> {item.PREFIXO}{" "}
+                      {item.DT_ABER} {item.DEFEITOS}
+                    </ul>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="container-orders" id="ELE">
+              <p>Setor Ordem</p>
+              <span>Nº OS Prefix Time Description</span>
+            </div>
+            <div className="container-orders" id="FUN">
+              <p>Setor Ordem</p>
+              <span>Nº OS Prefix Time Description</span>
+            </div>
+            <div className="container-orders" id="LUB">
               <p>Setor Ordem</p>
               <div className="orders">
                 <span>Nº OS Prefix Time Description</span>
@@ -62,28 +113,58 @@ function App() {
                 <span>Nº OS Prefix Time Description</span>
               </div>
             </div>
-            <div className="container-orders" id="ELE">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
-            </div>
-            <div className="container-orders" id="FUN">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
-            </div>
-            <div className="container-orders" id="LUB">
-              <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
-            </div>
             <div className="container-orders" id="MEC">
               <p>Setor Ordem</p>
-              <span>Nº OS Prefix Time Description</span>
+              <div className="orders">
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+                <br />
+                <span>Nº OS Prefix Time Description</span>
+              </div>
             </div>
             <div className="container-orders" id="TAP">
               <p>Setor Ordem</p>
               <span>Nº OS Prefix Time Description</span>
             </div>
           </div>
-          <div id="RET">
+          <div className="container-orders" id="RET">
             <p>OS Retorno</p>
           </div>
         </div>
@@ -116,7 +197,7 @@ function App() {
               <span>Nº OS Prefix Time Description</span>
             </div>
           </div>
-          <div id="RET">
+          <div className="container-orders" id="RET">
             <p>OS Retorno</p>
           </div>
         </div>
